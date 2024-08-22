a384fff4555d310809cc906200e070fc601befc2
js18user <93879664+js18user@users.noreply.github.com>
2024-08-22 23:21:59 +0300

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,7 +1,7 @@
 
 url_rabbit: str = "amqp://guest:guest@127.0.0.1/"
-url_rabbitmq: str = "amqps://xxhdmlgy:3VEb46SaBLY5eV7mVP_p0aKj7pMUUcy4@crow.rmq.cloudamqp.com/xxhdmlgy"
-
+url_rabbitmq_aws: str = "amqps://xxhdmlgy:3VEb46SaBLY5eV7mVP_p0aKj7pMUUcy4@crow.rmq.cloudamqp.com/xxhdmlgy"
+url_rabbit_google: str = "amqps://ilaieekx:2httl0idORgD1d_X4mc_nBYr7Hgxl3TZ@dog.lmq.cloudamqp.com/ilaieekx"
 url_keydb: str = "redis://localhost"
 url_redis: str = "redis://redis-12588.c226.eu-west-1-3.ec2.cloud.redislabs.com:18719"
 url_redi: str = "redis-rediscloud-corrugated:18719"
@@ -25,14 +25,8 @@ port: int = 5432
 host: str = 'localhost'
 
 query_many: str = f"INSERT INTO message (start_date,status,id_distribution,id_client) VALUES ($1,$2,$3,$4);"
-url_tws: str = "postgresql://gen_user:Aaa4401%26%26@77.73.132.189:5432/default_db"
-url_twp: str = "postgresql://gen_user:Bv(i%3D%26r5ILjTid@147.45.239.120:5432/default_db"
-db_user_tws: str = "gen_user"
-db_name_tws: str = "default_db"
-db_password_tws: str = "Aaa4401%26%26"
-db_port_tws: int = 5432
-db_host_tws: str = "77.73.132.189"
 
+url_twp: str = "postgresql://gen_user:Bv(i%3D%26r5ILjTid@147.45.239.120:5432/default_db"
 db_user_twp: str = "gen_user"
 db_name_twp: str = "default_db"
 db_password_twp: str = "Bv(i%3D%26r5ILjTid"
