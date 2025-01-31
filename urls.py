73d5ca4b2054409466428803b9ac8cbbf4a6a2de
js18user <93879664+js18user@users.noreply.github.com>
2025-01-31 23:09:54 +0200

    Add files via upload

--- urls.py
+++ urls.py
@@ -2,6 +2,7 @@
 url_rabbit: str = f"amqp://guest:guest@127.0.0.1/"
 url_rabbitmq_aws: str = f"amqps://xxhdmlgy:3VEb46SaBLY5eV7mVP_p0aKj7pMUUcy4@crow.rmq.cloudamqp.com/xxhdmlgy"
 url_rabbit_google: str = f"amqps://ilaieekx:2httl0idORgD1d_X4mc_nBYr7Hgxl3TZ@dog.lmq.cloudamqp.com/ilaieekx"
+
 url_keydb: str = f"redis://localhost"
 url_redis: str = f"redis://redis-12588.c226.eu-west-1-3.ec2.cloud.redislabs.com:18719"
 url_redi: str = f"redis-rediscloud-corrugated:18719"
@@ -26,13 +27,19 @@ host: str = f'localhost'
 
 query_many: str = f"INSERT INTO message (start_date,status,id_distribution,id_client) VALUES ($1,$2,$3,$4);"
 
-query_ratio: str = (f"SELECT SUM( heap_blks_hit ) / ( SUM(heap_blks_hit ) + SUM( heap_blks_read) ) as ratio "
-                    f"FROM pg_statio_user_tables ; "
+query_ratio: str = (f"SELECT SUM( heap_blks_hit ) / "
+                    f"( SUM(heap_blks_hit ) + SUM( heap_blks_read) ) "
+                    f"as ratio "
+                    f"FROM pg_statio_user_tables;"
 )
+query_set: str = f"SET max_parallel_workers_per_gather =  4 ;"
 
+# Postgresql 15  on TimeWeb Cloud
 url_twp: str = f"postgresql://gen_user:Bv(i%3D%26r5ILjTid@147.45.239.120:5432/default_db"
 db_user_twp: str = f"gen_user"
 db_name_twp: str = f"default_db"
 db_password_twp: str = f"Bv(i%3D%26r5ILjTid"
 db_port_twp: int = 5432
 db_host_twp: str = f"147.45.239.120"
+
+url_azure: str = f"postgresql://neondb_owner:npg_GoPNxHe0pzm4@ep-rapid-glitter-a9y5kqy5-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
