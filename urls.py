9ac7873b4f927a89ed6483eabd5f00800ff6fabf
js18user <93879664+js18user@users.noreply.github.com>
2025-09-01 01:22:20 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -30,7 +30,7 @@ user: str = f'gen_user'
 name: str = f'fintech'
 password: str = f'Aaa4401&&'
 port: int = 5432
-host: str = 192.168.0.4
+host: str = f'192.168.0.4'
 
 query_many: str = f"INSERT INTO message (start_date,status,id_distribution,id_client) VALUES ($1,$2,$3,$4);"
 
@@ -52,6 +52,7 @@ db_host_twp: str = f"147.45.239.120"
 # url_msp: str = f"postgresql://gen_user:********@192.168.0.4:5432/default_db"
 
 # url_azure: str = f"postgresql://neondb_owner:npg_GoPNxHe0pzm4@ep-rapid-glitter-a9y5kqy5-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
+
 
 
 
