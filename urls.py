0de4ad1c5e4168ddb33e8831a34099d03712a8bb
js18user <93879664+js18user@users.noreply.github.com>
2025-09-03 00:36:32 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -29,7 +29,7 @@ host: str = f'localhost'
 
 # Postgresql 17  on Local net
 # psql 'postgresql://gen_user:K5m_5u5tAB0%26lp@192.168.0.4:5432/default_db'
-urltw: str = 'postgresql://gen_user:K5m_5u5tAB0%26lp@192.168.0.4:5432/default_db'
+urltw: str = 'postgresql://gen_user:K5m_5u5tAB0%26lp@192.168.0.5:5432/default_db'
 
 user: str = f'gen_user'
 name: str = f'default_db'
@@ -57,6 +57,7 @@ db_host_twp: str = f"147.45.239.120"
 # url_msp: str = f"postgresql://gen_user:********@192.168.0.4:5432/default_db"
 
 # url_azure: str = f"postgresql://neondb_owner:npg_GoPNxHe0pzm4@ep-rapid-glitter-a9y5kqy5-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
+
 
 
 
