0069df26b1b86b9e22bf42298ff8d760aee64874
js18user <93879664+js18user@users.noreply.github.com>
2025-10-19 22:50:45 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -18,24 +18,12 @@ db_password_aws: str = f'p94bca2313004c8089b6cfcd9ec536c2a00e6212b58c63059e26298
 db_port_aws: int = 5432
 db_host_aws: str = f'cbu5eice9b7g15.cluster-czz5s0kz4scl.eu-west-1.rds.amazonaws.com'
 
-"""
-# Postgresql 17  on Local machine
+# Postgresql 15  on Local machine
 user: str = f'postgres'
 name: str = f'fintech'
 password: str = f'aa4401'
 port: int = 5432
 host: str = f'localhost'
-"""
-
-# Postgresql 17  on Local net
-# psql 'postgresql://gen_user:K5m_5u5tAB0%26lp@192.168.0.4:5432/default_db'
-urltw: str = 'postgresql://gen_user:K5m_5u5tAB0%26lp@192.168.0.5:5432/default_db'
-
-user: str = f'gen_user'
-name: str = f'default_db'
-password = "K5m_5u5tAB0&lp" 
-port: int = 5432
-host: str = f"192.168.0.4"
 
 query_many: str = f"INSERT INTO message (start_date,status,id_distribution,id_client) VALUES ($1,$2,$3,$4);"
 
@@ -54,29 +42,17 @@ db_password_twp: str = f"Bv(i%3D%26r5ILjTid"
 db_port_twp: int = 5432
 db_host_twp: str = f"147.45.239.120"
 
-# url_msp: str = f"postgresql://gen_user:********@192.168.0.4:5432/default_db"
+url_azure: str = (f"postgresql://neondb_owner:npg_GoPNxHe0pzm4@ep-rapid-glitter-a9y5kqy5-pooler.gwc."
+                  f"azure.neon.tech/neondb?sslmode=require")
 
-# url_azure: str = f"postgresql://neondb_owner:npg_GoPNxHe0pzm4@ep-rapid-glitter-a9y5kqy5-pooler.gwc.azure.neon.tech/neondb?sslmode=require"
+# Postgresql 15  on Local Timeweb
+'''
+user: str = f'postgres'
+name: str = f'msp'
+password: str = f'Aaa4401&&'
+port: int = 5432
+host: str = f'192.168.0.5'
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+# url_msp: str = f'postgresql://gen_user:********@192.168.0.4:5432/default_db'
+'''
+urltw: str = 'postgresql://gen_user:K5m_5u5tAB0%26lp@85.193.90.169:5432/default_db'
