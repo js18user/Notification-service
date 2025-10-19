92b4be29b58bb40403b4ce92c1f4468d3e63bfea
js18user <93879664+js18user@users.noreply.github.com>
2025-10-19 23:41:40 +0300

    Update urls.py

--- urls.py
+++ urls.py
@@ -46,13 +46,14 @@ url_azure: str = (f"postgresql://neondb_owner:npg_GoPNxHe0pzm4@ep-rapid-glitter-
                   f"azure.neon.tech/neondb?sslmode=require")
 
 # Postgresql 15  on Local Timeweb
-'''
+"""
 user: str = f'postgres'
 name: str = f'msp'
 password: str = f'Aaa4401&&'
 port: int = 5432
 host: str = f'192.168.0.5'
-
-# url_msp: str = f'postgresql://gen_user:********@192.168.0.4:5432/default_db'
-'''
+"""
+url_msp: str = f'postgresql://gen_user:********@192.168.0.4:5432/default_db'
+"""
 urltw: str = 'postgresql://gen_user:K5m_5u5tAB0%26lp@85.193.90.169:5432/default_db'
+"""
