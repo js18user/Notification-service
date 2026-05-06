62a0e6651c990a61ac22c806f42eaae5921792f5
js18user <93879664+js18user@users.noreply.github.com>
2026-05-06 23:27:41 +0300

    Create jit.py

---
+++ jit.py
@@ -0,0 +1,21 @@
+
+def jit_question():
+
+    import sys
+    from sysconfig import get_config_var
+
+    if hasattr(sys, "_jit") and sys._jit.is_available():
+        print("JIT is available in this Python build")
+        if sys._jit.is_enabled():
+            print("JIT is enabled and working!")
+        else:
+            print("JIT is disabled. Enable it using the PYTHON_JIT=1 variable.")
+    else:
+        print("This Python build does not have JIT support.")
+
+    flags = get_config_var("PY_CORE_CFLAGS") or ""
+    if "_Py_JIT" in flags:
+        print("Python is built with JIT support.")
+    else:
+        print("Python is built without JIT support.")
+    return
