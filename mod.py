ad73e8441d7cf7d1a360eeac32f381b7d3ee0e3d
js18user <93879664+js18user@users.noreply.github.com>
2026-06-05 22:32:11 +0300

    Update mod.py

--- mod.py
+++ mod.py
@@ -885,8 +885,8 @@ if __name__ == "__main__":
     try:
         # jit_question()
         run('mod:app', 
-            host='0.0.0.0', 
-            port=80, 
+            host='127.0.0.1', 
+            port=8000, 
             use_colors=True, 
             access_log=False,
             )
