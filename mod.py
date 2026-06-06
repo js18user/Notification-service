77f2891e609a1eb4eec050819c3a726bee56774c
js18user <93879664+js18user@users.noreply.github.com>
2026-06-07 00:30:19 +0300

    Update mod.py

--- mod.py
+++ mod.py
@@ -812,11 +812,8 @@ try:
     async def main(request: Request, db=Depends(get_db_connection), ):
         await db.execute("SELECT 1")
         host = request.headers.get("host", "")
-        if "resume.mcp-service.eu" in host or "cv.mcp-service.eu" in host:
-            return FileResponse("cv.pdf", 
-                                media_type="application/pdf",
-                                # headers={"Content-Disposition": "inline; filename=Jurijs_Satalovs_CV.pdf"},
-                               )
+        if ("resume.mcp-service.eu" in host or "cv.mcp-service.eu" in host):
+            return FileResponse(path="cv.pdf", media_type="application/pdf", )
         return FileResponse("data.html")
         
    @app.get('/admin/speed', status_code=200, description="Speed Api", )
