8a2d2df212d96546406b6b35db45a1ec41b6a101
js18user <93879664+js18user@users.noreply.github.com>
2026-06-25 14:53:29 +0300

    Update log.py

--- log.py
+++ log.py
@@ -40,7 +40,10 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 logger.remove()
-logger.add("app.json", serialize=Trueб rotation="10 MB", retention="1 day", )
+logger.add("app.json", 
+           serialize=True, 
+           rotation="10 MB", 
+           retention="1 day", )
 
 def websocket_log_sink(message):
     record = message.record
